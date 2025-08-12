8ba1c5d49712cf1589e862de93abe1a9f4e93e16
JayFretter <63405544+JayFretter@users.noreply.github.com>
2025-08-12 21:57:22 +0100

    Made record card animation shared by every card in list

--- components/shared/ExerciseRecordList.tsx
+++ components/shared/ExerciseRecordList.tsx
@@ -2,11 +2,12 @@ import ExerciseDefinition from "@/interfaces/ExerciseDefinition";
 import { SetPerformanceData } from "@/interfaces/ExercisePerformanceData";
 import RecordCard from "./RecordCard";
 import { roundHalf } from "@/utils/maths-utils";
-import { JSX } from "react";
+import { JSX, useEffect } from "react";
 import useCalculate1RepMax from "@/hooks/useCalculate1RepMax";
 import useCalculateVolume from "@/hooks/useCalculateVolume";
 import { WeightUnit } from "@/enums/weight-unit";
 import { View } from "react-native";
+import { useSharedValue, withRepeat, withTiming } from "react-native-reanimated";
 
 export interface ExerciseRecordListProps {
   exercise: ExerciseDefinition | null;
@@ -17,6 +18,13 @@ export interface ExerciseRecordListProps {
 export default function ExerciseRecordList({ exercise, currentPerformanceData, className }: ExerciseRecordListProps) {
   const calculateVolume = useCalculateVolume();
   const calculateOneRepMax = useCalculate1RepMax();
+  const sharedScale = useSharedValue(0);
+  const sharedOpacity = useSharedValue(1);
+
+  useEffect(() => {
+    sharedScale.value = withRepeat(withTiming(200, { duration: 3000 }), 2);
+    sharedOpacity.value = withRepeat(withTiming(0, { duration: 3000 }), 2);
+  }, []);
 
   const atLeastOneSetCompleted = () => {
     return currentPerformanceData.filter(s => s.type === 'weight').some(set => set.reps > 0);
@@ -25,7 +33,7 @@ export default function ExerciseRecordList({ exercise, currentPerformanceData, c
   const renderNewRecords = () => {
     if (currentPerformanceData.length === 0 && atLeastOneSetCompleted()) {
       return (
-        <RecordCard className='mt-4' title='1st time performing exercise' />
+        <RecordCard className='mt-4' title='1st time performing exercise' scale={sharedScale} opacity={sharedOpacity} />
       );
     }
 
@@ -35,7 +43,7 @@ export default function ExerciseRecordList({ exercise, currentPerformanceData, c
     const newVolume = roundHalf(calculateVolume(currentPerformanceData, WeightUnit.KG));
     if (newVolume > oldVolume) {
       records.push(
-        <RecordCard title='New volume record!' oldValue={oldVolume} newValue={newVolume} />
+        <RecordCard title='New volume record!' oldValue={oldVolume} newValue={newVolume} scale={sharedScale} opacity={sharedOpacity} />
       );
     }
 
@@ -43,7 +51,7 @@ export default function ExerciseRecordList({ exercise, currentPerformanceData, c
     const newEstimated1rm = roundHalf(calculateOneRepMax(currentPerformanceData, WeightUnit.KG));
     if (newEstimated1rm > oldEstimated1rm) {
       records.push(
-        <RecordCard title='New estimated 1 rep max!' oldValue={oldEstimated1rm} newValue={newEstimated1rm} />
+        <RecordCard title='New estimated 1 rep max!' oldValue={oldEstimated1rm} newValue={newEstimated1rm} scale={sharedScale} opacity={sharedOpacity} />
       );
     }
 
--- components/shared/RecordCard.tsx
+++ components/shared/RecordCard.tsx
@@ -1,26 +1,23 @@
 import AntDesign from "@expo/vector-icons/AntDesign";
 import { useEffect } from "react";
 import { Text, View } from "react-native";
-import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withTiming } from "react-native-reanimated";
+import Animated, { SharedValue, useAnimatedStyle, useSharedValue, withRepeat, withTiming } from "react-native-reanimated";
 
 export type RecordCardProps = {
   title: string;
   oldValue?: number;
   newValue?: number;
   className?: string;
+  scale?: SharedValue<number>;
+  opacity?: SharedValue<number>;
 }
 
-export default function RecordCard({ title, oldValue, newValue, className }: RecordCardProps) {
-  const circleScale = useSharedValue(0);
-  const circleOpacity = useSharedValue(1);
-
-  useEffect(() => {
-    circleScale.value = withRepeat(withTiming(200, { duration: 3000 }), -1);
-    circleOpacity.value = withRepeat(withTiming(0, { duration: 3000 }), -1);
-  }, [])
-
+export default function RecordCard({ title, oldValue, newValue, className, scale, opacity }: RecordCardProps) {
   const animatedCircleStyle = useAnimatedStyle(() => {
-    return { transform: [{ scale: circleScale.value }], opacity: circleOpacity.value };
+    return {
+      transform: [{ scale: scale ? scale.value : 1 }],
+      opacity: opacity ? opacity.value : 1,
+    };
   });
 
   return (
@@ -30,7 +27,7 @@ export default function RecordCard({ title, oldValue, newValue, className }: Rec
         <AntDesign name="Trophy" size={14} color="#3b82f6" />
         <Text className='text-txt-secondary'>{title}</Text>
       </View>
-      { oldValue !== undefined && newValue !== undefined && (
+      {oldValue !== undefined && newValue !== undefined && (
         <View className='flex-row items-center gap-2'>
           <Text className='text-txt-secondary'>{oldValue} kg</Text>
           <AntDesign name="arrowright" size={14} color="#3b82f6" />
